fd34b83e81db6a52452427f21a7e560922429c36
Jacek W <wozjac@zoho.com>
2021-05-27 00:32:52 +0200

    feat: remove dependencies to jQuery & jsdom

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,15 +1,12 @@
 import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 const input = ["src/ODataMockGenerator.js"];
-const banner_esm = "import { JSDOM } from \"jsdom\"; import _jQuery from \"jquery\"; " +
-  "const { window } = new JSDOM(\"\"); const jQuery = _jQuery(window);";
-const banner_cjs = "const { JSDOM } = require( \"jsdom\" ); const { window } = new JSDOM(\"\" ); " +
-  "const jQuery = require( \"jquery\" )( window );";
 
 export default [{
   input,
-  external: ["jsdom", "jquery"],
   plugins: [
+    nodeResolve(),
     commonjs(),
   ],
   output: [{
@@ -17,22 +14,12 @@ export default [{
       file: "dist/esm/bundle.js",
       sourcemap: true,
       exports: "named",
-      banner: banner_esm,
-      globals: {
-        jsdom: "JSDOM",
-        jquery: "jQuery"
-      }
     },
     {
       format: "cjs",
       file: "dist/cjs/index.js",
       sourcemap: true,
-      banner: banner_cjs,
       exports: "named",
-      globals: {
-        jsdom: "JSDOM",
-        jquery: "jQuery"
-      }
     }
   ]
 }];
