8400c87dd0437854ebd4f444a48895254ab36b37
Jacek W <wozjac@zoho.com>
2024-06-22 23:37:25 +0200

    build: switch to `@rollup/plugin-terser`

--- config/rollup.config.env.js
+++ config/rollup.config.env.js
@@ -1,37 +1,44 @@
 import commonjs from "@rollup/plugin-commonjs";
 import { babel } from "@rollup/plugin-babel";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 const input = ["src/ODataMockGenerator.js"];
 
-export default [{
-  input,
-  plugins: [
-    nodeResolve(),
-    commonjs(),
-    babel({
-      presets: [
-        ["@babel/env", {
-          useBuiltIns: "usage",
-          corejs: 3
-        }]
-      ],
-      babelHelpers: "bundled",
-      exclude: "node_modules/**"
-    }),
-    terser({
-      format: {
-        comments: /^!/,
-      }
-    })
-  ],
-  output: [{
-    format: "umd",
-    name: "omg",
-    esModule: false,
-    exports: "named",
-    sourcemap: true,
-    file: "dist/preset-env/bundle.min.js"
-  }]
-}];
+export default [
+  {
+    input,
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      babel({
+        presets: [
+          [
+            "@babel/env",
+            {
+              useBuiltIns: "usage",
+              corejs: 3,
+            },
+          ],
+        ],
+        babelHelpers: "bundled",
+        exclude: "node_modules/**",
+      }),
+      terser({
+        format: {
+          comments: /^!/,
+        },
+      }),
+    ],
+    output: [
+      {
+        format: "umd",
+        name: "omg",
+        esModule: false,
+        exports: "named",
+        sourcemap: true,
+        file: "dist/preset-env/bundle.min.js",
+      },
+    ],
+  },
+];
